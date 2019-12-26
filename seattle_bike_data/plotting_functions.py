905a72df630687ae8372e439ff9f6afe25189667
Doug Ollerenshaw <dougo@alleninstitute.org>
2019-12-26 14:58:19 -0800

    updates to plotting functions

--- seattle_bike_data/plotting_functions.py
+++ seattle_bike_data/plotting_functions.py
@@ -1,9 +1,10 @@
 import seaborn as sns
-import matplotlib.pyplot as pyplot
+import matplotlib.pyplot as plt
 import calendar
+import plotly.graph_objects as go
 
 
-def make_weekday_plot(df, palette=None):
+def make_weekday_plot_matplotlib(df, palette=None):
 
     f = sns.catplot(
         x='weekday',
@@ -24,17 +25,31 @@ def make_weekday_plot(df, palette=None):
 
     return f
 
-def make_monthly_plot(df, palette=None):
-    f = sns.catplot(
-        x='month',
-        y='total_crossings',
-        data=df.reset_index(),
-        kind='bar',
-        hue='year',
-        palette=palette,
-        legend=False
-    )
-    f.ax.set_xticklabels([calendar.month_name[d] for d in range(1,13)],rotation=45,ha='right')
+def make_monthly_plot_matplotlib(df, palette=None, groupby='year'):
+    if groupby == 'month':
+        f = sns.catplot(
+            x='month',
+            y='total_crossings',
+            data=df.reset_index(),
+            kind='bar',
+            hue='year',
+            palette=palette,
+            legend=False
+        )
+        f.ax.set_xticklabels([calendar.month_name[d] for d in range(1,13)],rotation=45,ha='right')
+
+
+    elif groupby == 'year':
+        f = sns.catplot(
+            x='year',
+            y='total_crossings',
+            data=df.reset_index(),
+            kind='bar',
+            hue='month',
+            palette=palette,
+            legend=False
+        )
+        
     f.ax.legend(loc='best')
     f.ax.set_ylabel('Total crossings')
     f.fig.set_size_inches(12,5)
@@ -42,3 +57,13 @@ def make_monthly_plot(df, palette=None):
     sns.despine()
 
     return f
+
+def make_rolling_yearly_plot_matplotlib(df):
+    fig,ax=plt.subplots(figsize=(12,5))
+    ax.plot(df['date'],df['total'],linewidth=3,color='black')
+    ax.set_xlabel('date')
+    ax.set_ylabel('sum over past 365 days')
+    ax.set_title('rolling yearly total crossings')
+    sns.despine()
+    fig.tight_layout()
+    return fig
